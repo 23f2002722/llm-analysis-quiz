639366a88952bafe570b17d341169bb4582a500a
23f2002722 <23f2002722@ds.study.iitm.ac.in>
2025-11-29 11:39:06 +0530

    Add directJsonSolver to module exports

--- solvers/index.js
+++ solvers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  directJsonSolver: require('./direct_json_solver'),
   base64Solver: require('./base64_solver'),
   pdfSumSolver: require('./pdf_sum_solver'),
   csvSolver: require('./csv_solver')
